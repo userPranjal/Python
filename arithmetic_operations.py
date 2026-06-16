4d652d4aed0b4da01c7b271a65941a2113e89c2a
Pranjal Desale <pranjaldesale120@gmail.com>
2026-06-16 21:39:27 +0530

    Add arithmetic_operations.py and next_year_age.py

---
+++ arithmetic_operations.py
@@ -0,0 +1,17 @@
+print("Enter first number:")
+No1 = int(input())
+
+print("Enter second number")
+No2 = int(input())
+
+addition = No1 + No2
+print("Addition =", addition)
+
+subtraction = No1 - No2
+print("Subtraction =", subtraction)
+
+multiplication = No1 * No2
+print("Multiplication =", multiplication)
+
+division = No1 / No2
+print("Division =", division)
---
+++ next_year_age.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+
+print("Enter your age: ")
+age = int(input())
+
+print("Hello", name + ", you will turn", age + 1, "next year.")
