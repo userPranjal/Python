d4e77a131882e37214f22519760ca4d9074044b8
userPranjal <pranjaldesale@gmail.com>
2026-07-13 22:20:16 +0530

    Sum of all odd numbers using multiprocessing.Pool

---
+++ sum_of_odd_numbers_pool.py
@@ -0,0 +1,28 @@
+import multiprocessing
+import os
+
+def OddSum(num):
+    Sum = 0
+
+    for i in range(1, num + 1, 2):
+        Sum = Sum + i
+
+    print("Process ID :", os.getpid())
+    print("Input Number :", num)
+    print("Sum od Odd Numbers :", Sum)
+    print()
+
+    return Sum
+
+def main():
+    Data = [1000000, 2000000, 3000000, 4000000]
+
+    pobj = multiprocessing.Pool()
+
+    result = pobj.map(OddSum, Data)
+
+    pobj.close()
+    pobj.join()
+
+if __name__ == "__main__":
+    main()
