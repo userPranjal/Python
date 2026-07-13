1501baa01bf897a5f4e0b45ff99002132d94ce2d
userPranjal <pranjaldesale@gmail.com>
2026-07-13 18:59:35 +0530

    Update factorial_using_pool.py

---
+++ factorial_using_pool.py
@@ -0,0 +1,28 @@
+import multiprocessing
+import os
+
+def Factorial(num):
+    result = 1
+    for i in range(1, num + 1):
+        result *= i
+
+    print("Process ID:", os.getpid())
+    return result
+
+def main():
+    Data = [10,15,20,25]
+
+    pobj = multiprocessing.Pool()
+
+    Result = pobj.map(Factorial, Data)
+
+    pobj.close()
+    pobj.join()
+
+    print("Input Number : ",Data)
+    print("Factorial :",Result)
+
+if __name__ == "__main__":
+    main()
+
+  
