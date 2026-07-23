4a2c3c923fe76aebdd4e341e5539af87b0d89f98
userPranjal <pranjaldesale120@gmail.com>
2026-07-23 19:17:06 +0530

    Add hourly file backup automation

---
+++ file_backup_scheduler.py
@@ -0,0 +1,44 @@
+import schedule
+import time
+import shutil
+import os
+import datetime
+
+def BackupFile(source, destination):
+
+    filename = os.path.basename(source)
+
+    index = filename.rfind(".")
+
+    timestamp = datetime.datetime.now().strftime("%d-%m-%Y_%I-%M-%S_%p")
+
+    backupfilename = filename[:index] + "_" + timestamp + filename[index:]
+
+    destinationpath = os.path.join(destination, backupfilename)
+
+    shutil.copy(source, destinationpath)
+
+    file = open("backup_log.txt", "a")
+    file.write("Backup completed successfully at : ")
+    file.write(datetime.datetime.now().strftime("%d-%m-%Y %I:%M:%S %p\n"))
+    file.close()
+
+    print("Backup completed successfully.")
+
+def main():
+
+    print("Automation Script Started")
+
+    source = input("Enter source file path : ")
+    destination = input("Enter destination directory path : ")
+
+    #schedule.every().hour.do(BackupFile, source, destination)
+
+    schedule.every(1).seconds.do(BackupFile, source, destination)
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+if __name__ == "__main__":
+    main()
